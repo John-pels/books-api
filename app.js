7390aa3a39634e99a0e752426e850a8cc8e276d9
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-11-16 16:54:06 +0100

    I implement a GET request

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const app = express();
+const bookRouter = express.Router()
+const db = mongoose.connect('mongodb://localhost/bookAPI')
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel')
+
+
+//Get all books
+
+bookRouter.route('/books')
+  .get((req,res) => {
+    const query = {};
+
+    if (req.query.genre) {
+      query.genre = req.query.genre
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err)
+      }
+      return res.json(books)
+    })
+  });
+
+
+  //Get a Book by ID
+
+bookRouter.route("/books/:bookId")
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  });
+
+app.use('/api', bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to My Express API!");
