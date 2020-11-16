cd371e6e82045f1f621aca99cb3d6834712da46a
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2020-11-16 18:54:58 +0100

    I clean up the code basr

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 const express = require("express");
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
-const bookRouter = express.Router()
 const db = mongoose.connect('mongodb://localhost/bookAPI')
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel')
+const bookRouter = require('./routes/bookRouter')(Book);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//Get all books
-
-bookRouter.route('/books')
-  .get((req,res) => {
-    const query = {};
-
-    if (req.query.genre) {
-      query.genre = req.query.genre
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err)
-      }
-      return res.json(books)
-    })
-  });
-
-
-  //Get a Book by ID
-
-bookRouter.route("/books/:bookId")
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 
 app.use('/api', bookRouter);
 
